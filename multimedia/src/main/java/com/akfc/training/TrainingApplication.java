148990362d3da3c33a619d31d61e5d6d2261e0ca
akoudri <ali.koudri@gmail.com>
2024-10-23 02:24:04 +0200

    Fixes gateway issue

--- multimedia/src/main/java/com/akfc/training/TrainingApplication.java
+++ multimedia/src/main/java/com/akfc/training/TrainingApplication.java
@@ -2,8 +2,10 @@ package com.akfc.training;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class TrainingApplication {
 
 	public static void main(String[] args) {
