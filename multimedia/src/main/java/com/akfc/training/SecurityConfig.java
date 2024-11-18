671cdb384e480ab2076b97154d5d109967ef8034
akoudri <ali.koudri@gmail.com>
2024-11-18 11:28:06 +0100

    Fixes configuration issues

--- multimedia/src/main/java/com/akfc/training/SecurityConfig.java
+++ multimedia/src/main/java/com/akfc/training/SecurityConfig.java
@@ -2,12 +2,15 @@ package com.akfc.training;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
